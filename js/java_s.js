14a1210fab48938cfc9b026f308164eab66d2f63
unknown <rafeen1305099@gmail.com>
2020-04-28 15:40:20 +0600

    first commit

---
+++ js/java_s.js
@@ -0,0 +1,28 @@
+function my_intro() {
+    var elmnt = document.getElementById("introduction_");
+    elmnt.scrollIntoView();
+}
+function my_education() {
+    var elmnt = document.getElementById("education_");
+    elmnt.scrollIntoView();
+}
+function my_skill() {
+    var elmnt = document.getElementById("skill_");
+    elmnt.scrollIntoView();
+}
+function my_projects() {
+    var elmnt = document.getElementById("projects_");
+    elmnt.scrollIntoView();
+}
+function my_thesis() {
+    var elmnt = document.getElementById("thesis_");
+    elmnt.scrollIntoView();
+}
+function my_contact() {
+    var elmnt = document.getElementById("contact_");
+    elmnt.scrollIntoView();
+}
+function my_additional() {
+    var elmnt = document.getElementById("additional_");
+    elmnt.scrollIntoView();
+}
